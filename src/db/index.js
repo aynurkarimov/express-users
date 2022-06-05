572b7754ae9dc908afc672a15f75a6ce6f13a176
Aynur Karimov <imaynurkarimov@gmail.com>
2022-06-05 15:44:02 +0300

    init: start the project

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    id: 0,
+    name: 'Alex',
+    age: 32,
+  },
+  {
+    id: 1,
+    name: 'Bred',
+    age: 15,
+  },
+  {
+    id: 2,
+    name: 'Ted',
+    age: 71,
+  },
+]
